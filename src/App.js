257e85fd9a913e0ef48a4a0e198cdedb3e0c56c5
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-02 14:54:07 -0400

    links and routes to audience components wired up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./App.css";
 import Audience from './components/audience'
+import AudienceJoinPres from './components/AudienceJoinPres'
+import { Link } from 'react-router-dom';
 
 class App extends Component {
  
@@ -9,9 +11,16 @@ class App extends Component {
       
       
     return (
-    <>
-     <Audience/>
-    </>
+      <Router>
+      <Route path="/" exact>
+        
+        <Link to={`/audience/`}>Audience </Link>
+        
+        
+      </Route>
+      <Route path="/audience" component={Audience} />
+      <Route path="/audience/join-presentation" component={AudienceJoinPres} />
+    </Router>
          
                      
     )
--- src/components/audience.jsx
+++ src/components/audience.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import AudienceJoinPres from './AudienceJoinPres'
 import "../App.css";
+import { Link } from 'react-router-dom';
 
 class Audience extends Component {
   
@@ -11,7 +12,9 @@ class Audience extends Component {
       
       return (
       <>
-       <AudienceJoinPres/>
+       <Link to={`/audience/join-presentation`}>
+        Join A Presentation
+      </Link>
       </>
            
                        
