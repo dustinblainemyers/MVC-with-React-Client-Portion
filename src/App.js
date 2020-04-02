d44525f6b0928e214edcc83413dd012fb1774662
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-02 13:13:05 -0400

    app.js component is now fetching data to express backend and logging to console

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+  state = {
+    
+    presentations: []
+  };
+
+  async componentDidMount() {
+    
+    const response = await fetch(`http://localhost:3333/join-presentation`);
+    const data = await response.json();
+
+    this.setState({
+        presentations: [this.state.presentations, data]
+      });
+  
+    }
+  
+    render() {
+      const {presentations} = this.state
+      console.log("api response", presentations)
+      
+      return (
+        
+           
+                     <p>hello</p>   
+                 
+            
+  
+        
+    )
+  
+    }
+  }
+  
+  export default App
+
+
+//   render() {
+//     const {presentations} = this.state
+//     return (
+      
+//       {presentations.length > 0 ? (
+        
+//         presentations.map((presentation,index) => (
+//             <p>
+//              {presentation.id}
+  
+//             </p>
+          
+
+            
+//         ))
+//       ) : (
+//         <li>No User Data</li>
+//       )}
+     
+//     );
+//   }
+// }
+
+// export default App;
