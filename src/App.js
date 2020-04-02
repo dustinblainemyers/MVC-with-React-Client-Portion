d1e59cfdd1e4efc46df6b540e02ef95c501a4f49
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-02 14:35:28 -0400

    audience components created

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import "./App.css";
+import Audience from './components/audience'
 
 class App extends Component {
-  state = {
+ 
+  render() {
+      
+      
+    return (
+    <>
+     <Audience/>
+    </>
+         
+                     
+    )
     
-    presentations: []
-  };
-
-  async componentDidMount() {
-    
-    const response = await fetch(`http://localhost:3333/join-presentation`);
-    const data = await response.json();
-
-    this.setState({
-        presentations: data
-      });
-  
-    }
-  
-    render() {
-      const {presentations} = this.state
-      console.log("api response 1", presentations[1])
-      
-      return (
-      <>
-        {presentations.length > 0 ? (
-          presentations.map(presentation => (
-            
-            <p>{presentation.id} hello</p>
-      
-          ))
-        ) : (
-           <p>no data</p>
-        )
-      }
-      </>
-           
-                       
-      )
-      
-    }
+  }
 
 }
+
 
 
 export default App;
---
+++ src/components/audience.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AudienceJoinPres from './AudienceJoinPres'
+import "../App.css";
+
+class Audience extends Component {
+  
+  
+    render() {
+      
+      
+      return (
+      <>
+       <AudienceJoinPres/>
+      </>
+           
+                       
+      )
+      
+    }
+
+}
+
+
+export default Audience;
+  
