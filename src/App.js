5f8215307d778ffe1f4827cc0c5b330e73ba42e6
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-02 13:56:31 -0400

    issue with fetch corrected

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     const data = await response.json();
 
     this.setState({
-        presentations: [this.state.presentations, data]
+        presentations: data
       });
   
     }
@@ -26,7 +26,7 @@ class App extends Component {
       return (
       <>
         {presentations.length > 0 ? (
-          presentations[1].map(presentation => (
+          presentations.map(presentation => (
             
             <p>{presentation.id} hello</p>
       
