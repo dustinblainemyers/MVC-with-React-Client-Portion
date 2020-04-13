8305a5afb947ec88d8cdecce8af457da0613980b
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-12 20:51:20 -0400

    removed unused imports

--- src/components/audience.jsx
+++ src/components/audience.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import AudienceJoinPres from './AudienceJoinPres'
 import "../App.css";
 import Nav from './nav'
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 
 import "../App.css";
-import { Link } from 'react-router-dom';
+
 
 class Nav extends Component {
   
