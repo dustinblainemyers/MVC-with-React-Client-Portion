dd7b9aa3dda680644296917a4d8950f6d0f10a5b
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-14 17:58:38 -0400

    I am able to console log auth0 user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./App.css";
-import Audience from './components/audience'
+
 import AudienceJoinPres from './components/AudienceJoinPres'
 import Presenter from './components/presenter'
 
@@ -9,23 +11,35 @@ import Nav from './components/nav'
 
 function App(props) {
   
-  
+  const { loading, user } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
       const Links = [
         {href: "/presenter", name: "Presenter"},
         {href: "/audiences" , name: "Audience"}
      ]
+     console.log("user",user.email)
       
     return (
       <Router>
       <Route path="/" exact>
       <div className="container">
+      <NavBar/>
+
       <h1>Green Light Red Light</h1>
+    
+      {props.email}
       <Nav link={Links}/>
+
+
       </div>
         
         
       </Route>
-      <Route path="/audiences" component={Audience} />
+      
       <Route path="/audience/join-presentation/:user_id?" component={AudienceJoinPres} />
       <Route path="/presenter" component={Presenter} />
     </Router>
