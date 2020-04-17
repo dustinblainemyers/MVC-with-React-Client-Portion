b7ae0098fafbdda3d19d18bf9683a61ac0bf05d7
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-17 00:42:45 -0400

    aggregate light updating properly and also able to pass in a query string

---
+++ src/components/Aggregate.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+class Aggregate extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      
+      aggregateLight: "",
+      test: 8,
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(`127.0.0.1:4001?token=${this.state.test}`);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+    
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+        <>
+        <p>aggregateLight:  {response} </p>
+        </>
+             
+                         
+        )
+  }
+}
+
+export default Aggregate;
