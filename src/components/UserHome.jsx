31fef93e5730ded795aecbc661e4f2da208534f6
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-22 15:21:20 -0400

    started adding materialize components. also installed materialize-css

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,66 +1,52 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import LogInOut from "./LogInOut";
-import AllHosting from "./AllHosting"
+import AllHosting from "./AllHosting";
 import Participating from "./Participating";
-import ViewUnjoined from "./ViewUnjoined"
-
-
-
+import ViewUnjoined from "./ViewUnjoined";
+import { Row, Col, Card } from "react-materialize";
 
 const UserHome = () => {
   const { user } = useAuth0();
-  const [user_id, setUserID] = useState([])
- 
+  const [user_id, setUserID] = useState([]);
 
   async function callApi() {
-    if(user.email === false) {
+    if (user.email === false) {
       return false;
     }
     try {
-    const response = await fetch(`http://localhost:3333/users/${user.email}`);
-    const data = await response.json();
-   
-    setUserID(data.id);} 
-    // setUserID(1)}
-    
-    catch {
-      console.log("there was an error with an api call in UserHome ")
+      const response = await fetch(`http://localhost:3333/users/${user.email}`);
+      const data = await response.json();
+
+      setUserID(data.id);
+    } catch {
+      // setUserID(1)}
+
+      console.log("there was an error with an api call in UserHome ");
     }
-    
   }
-  
-  useEffect(()  => {
-        
-   
-   
+
+  useEffect(() => {
     callApi();
-    
-   
-  
-    }, [user_id])
+  }, [user_id]);
 
   return (
-    <div>
-    
-     
+    <Row>
+      <Col>
+        <h1>Green Light Red Light</h1>
 
-     
-     <h1>Green Light Red Light</h1>
-     
-     <hr></hr>
-     <p>Logged in as :{user.email}</p>
-     <center><LogInOut/></center>
-     <AllHosting/>
-     <Participating user_id={user_id}/>
-     <ViewUnjoined user_id="8" />
-     
-    
-      
-    
-
-    </div>
-    
+        <hr></hr>
+        <p>Logged in as :{user.email}</p>
+        <center>
+          <LogInOut />
+        </center>
+        <Card>
+          <AllHosting />
+        </Card>
+        <Participating user_id={user_id} />
+        <ViewUnjoined user_id='8' />
+      </Col>
+    </Row>
   );
 };
 
