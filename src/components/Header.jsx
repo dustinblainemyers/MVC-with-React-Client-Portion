f6357e24bcbdddb64cbf9ee90c8e7acb827cf37f
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-29 12:55:03 -0400

    header now reads Status , centered and fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,27 @@ import "../App.css";
 
 function Header() {
   return (
-    <nav>
-      <div class='nav-wrapper'>
-        <a href='#' class='brand-logo center'>
-          Logo
-        </a>
-        <ul id='nav-mobile' class='left hide-on-med-and-down'>
-          <li>
-            <a href='sass.html'>Sass</a>
-          </li>
-          <li>
-            <a href='badges.html'>Components</a>
-          </li>
-          <li>
-            <a href='collapsible.html'>JavaScript</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div class='navbar-fixed'>
+      <nav>
+        <div class='nav-wrapper'>
+          {/* need get rid of link but keep styling */}
+          <a href='#' class='brand-logo center'>
+            Status
+          </a>
+          {/* <ul id='nav-mobile' class='left hide-on-med-and-down'>
+        <li>
+          <a href='sass.html'>Sass</a>
+        </li>
+        <li>
+          <a href='badges.html'>Components</a>
+        </li>
+        <li>
+          <a href='collapsible.html'>JavaScript</a>
+        </li>
+      </ul> */}
+        </div>
+      </nav>
+    </div>
   );
 }
 
